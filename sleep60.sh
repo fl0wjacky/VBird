e26cfe64b189529df51ab57ce7cf8b7219d83d46
ld <ld@jialuo.net>
2013-12-17 10:30:14 +0800

    nohup

---
+++ sleep60.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# Learn to use this command--nohup
+# in the bash shell enter this command :nohup ./sleep60.sh &
+# then logout and login to see the content of the file nohup.out
+
+/bin/sleep 60s
+/bin/echo "I sleep 60 seconds"
